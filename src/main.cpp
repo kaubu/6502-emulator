a1c18bb8030b4974d954c2aa4b51202f41f74436
kaubu <29447633+kaubu@users.noreply.github.com>
2023-08-28 23:44:05 +0000

    Memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using Byte = unsigned char;     // 8 bits
+using Word = unsigned short;    // 16 bits
+
+using u32 = unsigned int;
+
+struct Mem
+{
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialize()
+    {
+        for (u32 i = 0; i < MAX_MEM; i++)
+        {
+            Data[i] = 0;
+        }
+    }
+};
+
 struct CPU
 {
-    using Byte = unsigned char;     // 8 bits
-    using Word = unsigned short;    // 16 bits
-
     Word PC;    // Program Counter
     Word SP;    // Stack Pointer
 
@@ -19,10 +35,22 @@ struct CPU
     Byte B : 1;     // Break Command
     Byte V : 1;     // Overflow Flag
     Byte N : 1;     // Negative Flag
+
+    void Reset(Mem& memory)
+    {
+        PC = 0xFFFC;
+        SP = 0x0100;
+        // Clear flags
+        C = Z = I = D = B = V = N = 0;
+        A = X = Y = 0;
+        memory.Initialize();
+    }
 };
 
 int main()
 {
+    Mem mem;
     CPU cpu;
+    cpu.Reset(mem);
     return 0;
 }
